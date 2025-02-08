d6f284fbef84ad58c2ce46589def0d017aada88f
Jake Abed <jakeabed@proton.me>
2025-02-07 23:54:40 -0800

    Fix smol bugs in lexer

--- lexer.zig
+++ lexer.zig
@@ -18,7 +18,7 @@ const Lexer = struct {
                 if (self.peekChar() == '=') {
                     const ch = self.ch;
                     self.readChar();
-                    var l = [_]u8{ ch, ch };
+                    var l = [_]u8{ ch, self.ch };
                     const literal = l[0..];
                     tok = token.Token{
                         .type = token.TokenType.EQ,
@@ -29,23 +29,38 @@ const Lexer = struct {
                 } else {
                     var l = [_]u8{self.ch};
                     const literal = l[0..];
-                    tok = newToken(
-                        token.TokenType.ASSIGN,
-                        literal,
-                        self.line,
-                        self.col,
-                    );
+                    tok = self.newToken(token.TokenType.ASSIGN, literal);
                 }
             },
             '-' => {
                 var l = [_]u8{self.ch};
                 const literal = l[0..];
+                tok = self.newToken(token.TokenType.MINUS, literal);
+            },
+            '+' => {
+                var l = [_]u8{self.ch};
+                const literal = l[0..];
                 tok = token.Token{
-                    .type = token.TokenType.MINUS,
+                    .type = token.TokenType.PLUS,
                     .literal = literal,
                     .line = self.line,
                     .col = self.col,
                 };
+            },
+            '!' => {
+                var l = [_]u8{self.ch};
+                const literal = l[0..];
+                tok = self.newToken(token.TokenType.BANG, literal);
+            },
+            '*' => {
+                var l = [_]u8{self.ch};
+                const literal = l[0..];
+                tok = self.newToken(token.TokenType.ASTERISK, literal);
+            },
+            '/' => {
+                var l = [_]u8{self.ch};
+                const literal = l[0..];
+                tok = self.newToken(token.TokenType.SLASH, literal);
             },
             else => {
                 var l = [_]u8{0};
@@ -59,10 +74,12 @@ const Lexer = struct {
             },
         }
 
+        self.readChar();
+
         return tok;
     }
     fn readChar(self: *Lexer) void {
-        if (self.readPosition > self.input.len) {
+        if (self.readPosition >= self.input.len) {
             self.ch = 0;
         } else {
             self.ch = self.input[@intCast(self.readPosition)];
@@ -89,24 +106,18 @@ const Lexer = struct {
             }
         }
     }
+    fn newToken(self: *Lexer, t: token.TokenType, literal: []u8) token.Token {
+        return token.Token{
+            .type = t,
+            .literal = literal,
+            .col = self.col,
+            .line = self.line,
+        };
+    }
 };
 
 pub fn init(inp: []u8) *Lexer {
     var l = Lexer{ .input = inp };
     l.readChar();
     return &l;
-}
-
-pub fn newToken(
-    tokType: token.TokenType,
-    literal: []u8,
-    line: u64,
-    col: u64,
-) token.Token {
-    return token.Token{
-        .type = tokType,
-        .literal = literal,
-        .line = line,
-        .col = col,
-    };
 }
--- main.zig
+++ main.zig
@@ -2,11 +2,15 @@ const std = @import("std");
 const lexer = @import("lexer.zig");
 
 pub fn main() void {
-    var literal = " == -".*;
+    var literal = " == - + *".*;
     const cook = literal[0..];
     const l = lexer.init(cook);
     const tok = l.nextToken();
     const tok2 = l.nextToken();
+    const tok3 = l.nextToken();
+    const tok4 = l.nextToken();
     std.debug.print("{s} {}\n", .{ tok.literal, tok.type });
     std.debug.print("{s} {}\n", .{ tok2.literal, tok2.type });
+    std.debug.print("{s} {}\n", .{ tok3.literal, tok3.type });
+    std.debug.print("{s} {}\n", .{ tok4.literal, tok4.type });
 }
